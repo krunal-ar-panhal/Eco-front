b1ccd3687e43403908c3b3998fda63bf79089eb3
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-09-27 11:46:04 +0530

    create important pages and components also setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Collection from "./Pages/Collection";
+import Order from "./Pages/Order";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Product from "./Pages/Product";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/product/:productId" element={<Product />} />
+        </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
