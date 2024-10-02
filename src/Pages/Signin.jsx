09125a1300c7e69319f5dd83d6cfff68907a031b
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-10-02 13:17:53 +0530

    create signin and signup page in single page

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import Motion from "../Components/Motion";
 
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+  const [currentState, setCurrentState] = useState("Sign In");
 
-export default Signin
+  const handleSubmit = (e) => {
+    e.preventdefault()
+  }
+
+  return (
+    <Motion>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+        <div className="inline-flex items-center gap-2 mb-2 mt-10">
+          <p className="prata-regular text-3xl ">{currentState}</p>
+          <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+        </div>
+        {currentState === "Sign In" ? (
+          ""
+        ) : (
+          <input
+            type="text"
+            placeholder="Name"
+            required
+            className="w-full px-3 py-2 border border-gray-800"
+          />
+        )}
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          className="w-full px-3 py-2 border border-gray-800"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className="w-full px-3 py-2 border border-gray-800"
+        />
+        <div className="w-full flex justify-between text-sm mt-[8px]">
+          <p className="cursor-pointer"> Forget your password</p>
+          {currentState === "Sign In" ? (
+            <p onClick={()=>setCurrentState("Sign Up")} className="cursor-pointer">Create Account</p>
+          ) : (
+            <p onClick={()=>setCurrentState("Sign In")} className="cursor-pointer">Login here</p>
+          )}
+        </div>
+        <button className="bg-black text-white font-light px-8 py-2 mt-4">{currentState === "Sign In" ? "Good To Go In" : "Good To Go Up" }</button>
+      </form>
+    </Motion>
+  );
+};
+
+export default Signin;
